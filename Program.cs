7e26a4b9ceb2a044834dfbeea743cb5bd45465ab
IVAN-SMI <Van96.Smirnov@gmail.com>
2024-09-23 23:11:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+    double par1;
+    double par2;
+    double result = 0;
+    char oper;
+
+    Console.WriteLine("Введите первое число");
+    par1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    par2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите символ математического действия");
+    oper = Convert.ToChar(Console.ReadLine());
+
+if (oper == '+')
+{
+    result = par1 + par2;
+    Console.WriteLine($"{par1} {oper} {par2} = {result}");
+}
+
+else if (oper == '-')
+{
+    result = par1 - par2;
+    Console.WriteLine($"{par1} {oper} {par2} = {result}");
+}
+
+else if (oper == '*')
+{
+    result = par1 * par2;
+    Console.WriteLine($"{par1} {oper} {par2} = {result}");
+}
+
+else if (oper == '/')
+{
+    if (par2 == 0)
+        Console.WriteLine("Ошибка. На ноль делить нельзя");
+    else if (oper == '/')
+        result = par1 / par2;
+    Console.WriteLine($"{par1} {oper} {par2} = {result}");
+}
+else
+{
+    Console.WriteLine("Ошибка. Данную операцию выполнить невозможно");
+}
+
+Console.ReadKey();
